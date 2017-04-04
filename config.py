793f6197ad7349daa90b2e93ef0af55682b3f034
joaquinpunales1992 <joaquinpunales@gmail.com>
2017-04-04 12:59:48 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
-WPIversion = '2017-03-28'
-WPIusername = '681ec595-be4f-4733-9bfd-b1ca612007b7'
-WPIpass = 'hbP3pgmQxYHI'
+#Ask me to ge this values
 
-WTAversion = '2017-03-28'
-WTAusername = '7a5bc772-8c49-4c79-acf3-783e81df4237'
-WTApass = 'rJ5zZrduV1bg'
+WPIversion = ''
+WPIusername = ''
+WPIpass = ''
+
+WTAversion = ''
+WTAusername = ''
+WTApass = ''
