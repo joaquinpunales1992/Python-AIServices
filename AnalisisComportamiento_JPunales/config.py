31775edc897ca9015d0a3f384a3e8edcf4f5080a
joaquinpunales1992 <joaquinpunales@gmail.com>
2017-04-04 13:01:08 -0300

    Update config.py

--- AnalisisComportamiento_JPunales/config.py
+++ AnalisisComportamiento_JPunales/config.py
@@ -1,7 +1,8 @@
-WPIversion = '2017-03-28'
-WPIusername = '681ec595-be4f-4733-9bfd-b1ca612007b7'
-WPIpass = 'hbP3pgmQxYHI'
+#Ask me to get this values: joaquinpunales@gmail.com
+WPIversion = ''
+WPIusername = ''
+WPIpass = ''
 
-WTAversion = '2017-03-28'
-WTAusername = '7a5bc772-8c49-4c79-acf3-783e81df4237'
-WTApass = 'rJ5zZrduV1bg'
+WTAversion = ''
+WTAusername = ''
+WTApass = ''
